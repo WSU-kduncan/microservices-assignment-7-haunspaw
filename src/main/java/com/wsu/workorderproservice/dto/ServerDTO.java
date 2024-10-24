4550e946bafadee99f48b6f6e2e6f8d3a4450ae2
Harrison Aunspaw <haunspaw@gmail.com>
2024-10-24 10:15:15 -0400

    new file:   dto/ServerDTO.java 	new file:   model/Server.java 	new file:   repository/ServerRepository.java

---
+++ src/main/java/com/wsu/workorderproservice/dto/ServerDTO.java
@@ -0,0 +1,38 @@
+package com.wsu.workorderproservice.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ServerDTO {
+
+    private Integer id;
+    private Integer serverId;
+
+    @NotBlank(message = "First Name must not be null or blank")
+    @Size(max = 35)
+    private String firstName; 
+
+    @NotBlank(message = "First Name must not be null or blank")
+    @Size(max = 35)
+    private String lastName;
+
+    @NotBlank(message = "First Name must not be null or blank")
+    @Size(max = 10)
+    private String availability;
+
+}
